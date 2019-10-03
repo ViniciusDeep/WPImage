541aeecb8a89b8591925c379002f0dff943b1830
Vinicius Mangueira <viniciuscorreia11@gmail.com>
2019-10-02 21:29:00 -0300

    [Launch] - Start project

---
+++ WPImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WPImage
+//
+//  Created by Vinicius Mangueira on 02/10/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
